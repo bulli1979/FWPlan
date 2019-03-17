6e1edf17932a32c3638dfbed49133b3f3309b52e
bulli1979 <mail@mirko-eberlein.de>
2019-03-17 19:28:49 +0100

    change stuff to pull out texts not finished

--- src/main/java/screens/CreateOrEditPlan.java
+++ src/main/java/screens/CreateOrEditPlan.java
@@ -3,6 +3,7 @@ package screens;
 import java.util.UUID;
 
 import application.ApplicationHandler;
+import constants.Texts;
 import constants.ValueHolder;
 import data.Plan;
 import data.db.DBPlan;
@@ -30,10 +31,6 @@ public class CreateOrEditPlan implements ApplicationScreen{
 	
 	private Plan editPlan = null;
 	private VBox vBox = new VBox();
-	private final TextArea wassertransportTextField2=new TextArea();
-	private final TextArea wassertransportTextField3=new TextArea();
-	private final TextArea wassertransportTextField4=new TextArea();
-	
 	public CreateOrEditPlan() {
 	}
 	
@@ -43,43 +40,39 @@ public class CreateOrEditPlan implements ApplicationScreen{
 		BorderPane root = new BorderPane();
 		GridPane grid = new GridPane();
 		prepareGrid(grid);
-		int rowCount = 0;
-		
-		
-
-		Text scenetitle = new Text("Einsatzplan erstellen");
-		scenetitle.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		Integer rowCount = 0;
+		Text scenetitle = new Text(Texts.EDIT_OR_CREATE_TEXT);
+		scenetitle.setFont(Font.font(Texts.ARIAL_FONT, FontWeight.BOLD, 20));
 		grid.add(scenetitle, 0, rowCount, 2, 1);
-		rowCount+=2;
-		
-		Label planNumber = new Label("Plannummer:");
+		rowCount += 2;
+		Label planNumber = new Label(Texts.PLANNUMBER_TEXT);
 		grid.add(planNumber, 0, rowCount);
 		final TextField planNumberTextField = new TextField();
 		grid.add(planNumberTextField, 1, rowCount);
 		rowCount++;
-		Label title = new Label("Titel:");
+		Label title = new Label(Texts.TITLE_TEXT);
 		grid.add(title, 0, rowCount);
 		final TextField titleTextField = new TextField();
 		grid.add(titleTextField, 1, rowCount);
 		rowCount++;
-		Label description = new Label("Beschreibung:");
+		Label description = new Label(Texts.DESCRIPTION_TEXT);
 		grid.add(description, 0, rowCount);
 		final TextArea descriptionTextField = new TextArea();
 		grid.add(descriptionTextField, 1, rowCount);
 		rowCount++;
-		Label adresse = new Label("Adresse:");
+		Label adresse = new Label(Texts.ADRESS_TEXT);
 		grid.add(adresse, 0, rowCount);
 		final TextArea adresseTextField = new TextArea();
 		grid.add(adresseTextField, 1, rowCount);
 		rowCount++;
 		
-		Label sofortmassnahmen = new Label("Sofortmassnahmen:");
+		Label sofortmassnahmen = new Label(Texts.INSTANT_ACTION_TEXT);
 		grid.add(sofortmassnahmen, 0, rowCount);
 		final TextArea sofortmassnahmenTextField = new TextArea();
 		grid.add(sofortmassnahmenTextField, 1, rowCount);
 		rowCount++;
 		
-		Label wassertransportCounterLabel = new Label("Wieviele Wassertransporte:");
+		Label wassertransportCounterLabel = new Label(Texts.HOW_MANY_WATHER_TEXT);
 		grid.add(wassertransportCounterLabel, 0, rowCount);
 		ComboBox<Integer> wasserTransportCounter = new ComboBox<Integer>();
 		wasserTransportCounter.getItems().add(1);
@@ -89,26 +82,18 @@ public class CreateOrEditPlan implements ApplicationScreen{
 		wasserTransportCounter.setValue(1);
 		grid.add(wasserTransportCounter, 1, rowCount);
 		rowCount++;
+		
 		wasserTransportCounter.setOnAction(event ->{
 			paintWatherTransport(wasserTransportCounter.getValue());
 		});
-		
-		Label wassertransport = new Label("Wassertransport 1:");
-		grid.add(wassertransport, 0, rowCount);
-		final TextArea wassertransportTextField = new TextArea();
-		grid.add(wassertransportTextField, 1, rowCount);	
-		
 		vBox.getChildren().add(grid);
-		
-			
-		
+
 		paintWatherTransport(1);
-		
 		
 		rowCount=0;
 		GridPane grid2 = new GridPane();
 		prepareGrid(grid2);
-		Label wichtigeKontakte = new Label("Wichtige Kontakte:");
+		Label wichtigeKontakte = new Label(Texts.IMPORTANT_CONTACTS_TEXT);
 		grid2.add(wichtigeKontakte, 0, rowCount);
 		final TextArea wichtigeKontakteTextField = new TextArea();
 		grid2.add(wichtigeKontakteTextField, 1, rowCount);
@@ -119,14 +104,10 @@ public class CreateOrEditPlan implements ApplicationScreen{
 			descriptionTextField.setText(editPlan.getDescription());
 			adresseTextField.setText(editPlan.getAdress());
 			sofortmassnahmenTextField.setText(editPlan.getInstantAction());
-			wassertransportTextField.setText(editPlan.getWatherTransport());
 			wichtigeKontakteTextField.setText(editPlan.getImportantContacts());
 		}
-		
-		
-		
-		
-		Button btnSave = new Button("Speichern");
+
+		Button btnSave = new Button(Texts.SAVE_TEXT);
 		btnSave.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
@@ -139,7 +120,7 @@ public class CreateOrEditPlan implements ApplicationScreen{
 					withDescription(descriptionTextField.getText()).
 					withAdress(adresseTextField.getText()).
 					withInstantAction(sofortmassnahmenTextField.getText()).
-					withWatherTransport(wassertransportTextField.getText()).
+					withWatherTransport(getWatherValue(0)).
 					withImportantContact(wichtigeKontakteTextField.getText()).build();	
 					
 				if(editPlan == null){
@@ -151,9 +132,9 @@ public class CreateOrEditPlan implements ApplicationScreen{
 				editPlan = savePlan;
 				
 				Alert alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Einsatzplan Speicherung");
+				alert.setTitle(Texts.PLAN_SAVED_TEXT);
 				alert.setHeaderText(null);
-				alert.setContentText("Der Einsatzplan " + planNumberTextField.getText() + " wurde erfolgreich gespeichert.");
+				alert.setContentText(String.format(Texts.PLAN_SAVE_COMPLETE_TEXT, planNumberTextField.getText()));
 				alert.showAndWait();
 				
 				ApplicationHandler.setScreen(ScreenObject.$PLAN_LIST_SCREE);
@@ -168,46 +149,38 @@ public class CreateOrEditPlan implements ApplicationScreen{
 		root.setCenter(vBox);
 		return root;
 	}
-
+	
+	private String getWatherValue(int number) {
+		String ret = "";
+		TextArea area = (TextArea)vBox.getScene().lookup("#"+Texts.WHATERID_TEXT + number);
+		if(area != null) {
+			return area.getText();
+		}
+		return ret;
+	}
 	
 	private void paintWatherTransport(int value) {
-		int rowCount = 0;
-		System.out.println("val: " + value);
-		if(vBox.getChildren().size()==3) {
-			System.out.println("remove 1");
+		if(vBox.getChildren().size() == 3) {
 			vBox.getChildren().remove(1);
 		}
-		if(value>1) {
-			GridPane grid = new GridPane();
-			prepareGrid(grid);
-			Label wassertransport2 = new Label("Wassertransport 2:");
-			grid.add(wassertransport2, 0, rowCount);
-			grid.add(wassertransportTextField2, 1, rowCount);
-			if(value>2) {
-				rowCount++;
-				Label wassertransport3 = new Label("Wassertransport 3:");
-				grid.add(wassertransport3, 0, rowCount);
-				grid.add(wassertransportTextField3, 1, rowCount);
-			
-			}else {
-				wassertransportTextField3.setText("");
-			}
-			if(value>3) {
-				rowCount++;
-				
-				Label wassertransport4 = new Label("Wassertransport 4:");
-				grid.add(wassertransport4, 0, rowCount);
-				grid.add(wassertransportTextField4, 1, rowCount);
-		
-			}else {
-				wassertransportTextField4.setText("");
-			}	
-			System.out.println("add grid");
-			vBox.getChildren().add(1,grid);
-		}else {
-			wassertransportTextField2.setText("");
+		GridPane grid = new GridPane();
+		prepareGrid(grid);
+		int count = 0;
+		while(count < value) {
+			addWather(grid, count);
+			count++;
 		}
+		vBox.getChildren().add(1,grid);
 	}
+	
+	private void addWather(GridPane grid,int number) {
+		Label label = new Label(Texts.WATHERTRANSPORT + Texts.SPACE + (number+1));
+		grid.add(label, 0, number);
+		final TextArea watherTextArea = new TextArea();
+		watherTextArea.setId(Texts.WHATERID_TEXT + number);
+		grid.add(watherTextArea, 1, number);
+	}
+	
 	
 	private void prepareGrid(GridPane grid) {
 		grid.setAlignment(Pos.CENTER);
